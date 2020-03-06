494752187de367ad27bb9fa487eb527165633b3e
mohammadfakhani <mohammadfakhani@users.noreply.github.com>
2020-03-06 21:44:54 +0200

    Create CloseLoans.java

---
+++ src/main/java/com/example/CloseLoans/CloseLoans.java
@@ -0,0 +1,39 @@
+package com.example.CloseLoans;
+
+import com.example.Loans.Loans;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class CloseLoans {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int CloseLoanID ;
+	
+	@ManyToOne
+	private Loans loan=null;
+
+	public int getCloseLoanID() {
+		return CloseLoanID;
+	}
+
+	public void setCloseLoanID(int closeLoanID) {
+		CloseLoanID = closeLoanID;
+	}
+
+	public Loans getLoan() {
+		return loan;
+	}
+
+	public void setLoan(Loans loan) {
+		this.loan = loan;
+	}
+	
+	
+	
+}
