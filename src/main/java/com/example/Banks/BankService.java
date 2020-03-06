5b4c29f4e92713c09910373923d91ef1bfea08b4
mohammadfakhani <mohammadfakhani@users.noreply.github.com>
2020-03-06 21:44:41 +0200

    Update BankService.java

--- src/main/java/com/example/Banks/BankService.java
+++ src/main/java/com/example/Banks/BankService.java
@@ -6,8 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.example.dataBase.BanksRepository;
-import com.example.models.Banks;
+
 
 
 @Service
