1452a652364e8d3f628572969fcf92fd6c7fb949
mohammadfakhani <mohammadfakhani@users.noreply.github.com>
2020-03-06 21:46:05 +0200

    Create LoansType.java

---
+++ src/main/java/com/example/LoansType/LoansType.java
@@ -0,0 +1,37 @@
+package com.example.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class LoansType {
+
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int LoanTypeID ;
+	private String TypeName="";
+	
+	
+	public LoansType(String loanTypeName ) {
+		this.TypeName = loanTypeName ; 
+	}
+	
+	public int getLoanTypeID() {
+		return LoanTypeID;
+	}
+	public void setLoanTypeID(int loanTypeID) {
+		LoanTypeID = loanTypeID;
+	}
+	public String getTypeName() {
+		return TypeName;
+	}
+	public void setTypeName(String typeName) {
+		TypeName = typeName;
+	}
+	
+	
+	
+}
