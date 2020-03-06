978fa12121cb27257cf637cfa14022c1e112ae8d
mohammadfakhani <mohammadfakhani@users.noreply.github.com>
2020-03-06 21:44:32 +0200

    Update AllocationsRepository.java

--- src/main/java/com/example/Allocations/AllocationsRepository.java
+++ src/main/java/com/example/Allocations/AllocationsRepository.java
@@ -1,9 +1,8 @@
-package com.example.dataBase;
+package com.example.Allocations;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.example.models.Allocations;
 
 @Repository
 public interface AllocationsRepository extends JpaRepository<Allocations, Integer> {
