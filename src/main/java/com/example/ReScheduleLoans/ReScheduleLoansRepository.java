e7df6ee37d8f1d2b97c0ab29201cfeaf04aa2935
mohammadfakhani <mohammadfakhani@users.noreply.github.com>
2020-03-06 21:47:27 +0200

    Create ReScheduleLoansRepository.java

---
+++ src/main/java/com/example/ReScheduleLoans/ReScheduleLoansRepository.java
@@ -0,0 +1,10 @@
+package com.example.ReScheduleLoans;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface ReScheduleLoansRepository extends JpaRepository<ReScheduleLoans,Integer>{
+
+}
