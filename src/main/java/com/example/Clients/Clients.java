b64c2fe441a9869ca14c35c698411d1c12b8b928
mohammadfakhani <mohammadfakhani@users.noreply.github.com>
2020-03-25 14:29:57 +0200

    public

--- src/main/java/com/example/Clients/Clients.java
+++ src/main/java/com/example/Clients/Clients.java
@@ -1,51 +1,34 @@
 package com.example.Clients;
 
-import ValidContent_Visitor.Valid_Visitable;
 import ValidContent_Visitor.Visitor;
 
-
-import javax.persistence.Column;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.validation.constraints.Null;
+import javax.persistence.*;
 
 
 @Entity
 
-public class Clients extends Valid_Visitable {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private int ClientID;
-    public String ClientName;
-    public String identity_number;
 
 public class Clients {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private int ClientID ;
-	private String ClientName ;
-	private String ClientType;
-	@Column(nullable = true)
-	private String identity_number;
-	
-	
-	public Clients() {}
-
-	
-	
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public int ClientID;
+    public String ClientName;
+    public String ClientType;
+    @Column(nullable = true)
+    public String identity_number;
 
 
-	public Clients(String clientName, String clientType, String identity_number) {
-		
-		ClientName = clientName;
-		ClientType = clientType;
-		this.identity_number = identity_number;
-	}
+    public Clients() {
+    }
+
+
+    public Clients(String clientName, String clientType, String identity_number) {
+
+        ClientName = clientName;
+        ClientType = clientType;
+        this.identity_number = identity_number;
+    }
 
 
     public Clients(String clientName, String identity_number) {
@@ -58,26 +41,16 @@ public class Clients {
         return ClientID;
     }
 
-
-
-
-	public String getClientType() {
-		return ClientType;
-	}
-
-
-	public void setClientType(String clientType) {
-		ClientType = clientType;
-	}
-
-
-	public int getClientID() {
-		return ClientID;
-	}
-
-
     public void setClientID(int clientID) {
         ClientID = clientID;
+    }
+
+    public String getClientType() {
+        return ClientType;
+    }
+
+    public void setClientType(String clientType) {
+        ClientType = clientType;
     }
 
     public String getClientName() {
