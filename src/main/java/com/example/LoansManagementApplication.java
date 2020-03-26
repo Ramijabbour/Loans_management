372db0279c3ac1618e9541a6d1f1161b2be292a1
mohammadfakhani <mohammadfakhani@users.noreply.github.com>
2020-03-26 19:40:51 +0200

    l

---
+++ src/main/java/com/example/LoansManagementApplication.java
@@ -0,0 +1,60 @@
+package com.example;
+
+
+import ValidContent_Visitor.Valid_Visitable;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+
+import com.example.TT.tt;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class LoansManagementApplication implements CommandLineRunner {
+
+
+    final static String queueName = "message_queue";
+
+    public static void main(String[] args) throws InterruptedException {
+
+        SpringApplication.run(LoansManagementApplication.class, args);
+
+
+    }
+
+
+    @Override
+
+    public void run(String... args) throws Exception {
+
+//
+
+
+        Scanner newScan = new Scanner(System.in);
+
+
+        //اختبار ال visitor
+        Valid_Visitable valid_visitable = new Valid_Visitable();
+        tt t = new tt();
+        t.setTotal("1ahmad  attaR");
+        t.accept(valid_visitable);
+
+
+        // كيفية ادخال تاريخ
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-M-yyyy");
+        String dateInString = "31-08-1982";
+        Date date = sdf.parse(dateInString);
+        System.out.println(date);
+
+
+    }
+}
+
