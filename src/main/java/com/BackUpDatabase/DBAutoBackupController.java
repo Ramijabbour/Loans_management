48290b1e2b8ab9f39d42dc2940c1881f3c0ac492
Mohammed_Altujjar <mohammed_.1996@live.com>
2020-04-04 19:23:05 +0300

    دمج الملفات غلط اسحبوه ليزبط

--- src/main/java/com/BackUpDatabase/DBAutoBackupController.java
+++ src/main/java/com/BackUpDatabase/DBAutoBackupController.java
@@ -6,8 +6,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
+import javax.swing.*;
+import java.awt.*;
 import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.security.CodeSource;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -19,42 +23,91 @@ public class DBAutoBackupController implements CommandLineRunner {
     @Override
     @Scheduled(cron = "0 30 1 * * ?")
     public void run(String... args) throws Exception {
+    }
 
-        System.out.println("Backup Started at " + new Date());
-
-        Date backupDate = new Date();
-        SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy");
-        String backupDateStr = format.format(backupDate);
-        String dbNameList = "client_1 client_2";
-
-        String fileName = "Daily_DB_Backup";
-        String folderPath = "C:\\home";
-        File f1 = new File(folderPath);
-        f1.mkdir();
-
-        String saveFileName = fileName + "_" + backupDateStr + ".sql";
-        String savePath = filePath + File.separator + saveFileName;
-
-        String executeCmd = "mysqldump -u " + dbUserName + " -p" + dbUserPassword + "  --databases " + dbNameList
-                + " -r " + savePath;
-
-        Process runtimeProcess = null;
+    public static void Backupdbtosql() {
         try {
-            runtimeProcess = Runtime.getRuntime().exec(executeCmd);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        int processComplete = 0;
-        try {
-            processComplete = runtimeProcess.waitFor();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
 
-        if (processComplete == 0) {
-            System.out.println("Backup Complete at " + new Date());
-        } else {
-            System.out.println("Backup Failure");
+            /*NOTE: Getting path to the Jar file being executed*/
+            /*NOTE: YourImplementingClass-> replace with the class executing the code*/
+            CodeSource codeSource = DBAutoBackupController.class.getProtectionDomain().getCodeSource();
+            File jarFile = new File(codeSource.getLocation().toURI().getPath());
+            String jarDir = jarFile.getParentFile().getPath();
+
+
+            /*NOTE: Creating Database Constraints*/
+            String dbName = "YourDBName";
+            String dbUser = "YourUserName";
+            String dbPass = "YourUserPassword";
+
+            /*NOTE: Creating Path Constraints for folder saving*/
+            /*NOTE: Here the backup folder is created for saving inside it*/
+            String folderPath = jarDir + "\\backup";
+
+            /*NOTE: Creating Folder if it does not exist*/
+            File f1 = new File(folderPath);
+            f1.mkdir();
+
+            /*NOTE: Creating Path Constraints for backup saving*/
+            /*NOTE: Here the backup is saved in a folder called backup with the name backup.sql*/
+            String savePath = "\"" + jarDir + "\\backup\\" + "backup.sql\"";
+
+            /*NOTE: Used to create a cmd command*/
+            String executeCmd = "mysqldump -u" + dbUser + " -p" + dbPass + " --database " + dbName + " -r " + savePath;
+
+            /*NOTE: Executing the command here*/
+            Process runtimeProcess = Runtime.getRuntime().exec(executeCmd);
+            int processComplete = runtimeProcess.waitFor();
+
+            /*NOTE: processComplete=0 if correctly executed, will contain other values if not*/
+            if (processComplete == 0) {
+                System.out.println("Backup Complete");
+            } else {
+                System.out.println("Backup Failure");
+            }
+
+        } catch (URISyntaxException | IOException | InterruptedException ex) {
+            JOptionPane.showMessageDialog(null, "Error at Backuprestore" + ex.getMessage());
         }
     }
+
+    public static void Restoredbfromsql(String s) {
+        try {
+            /*NOTE: String s is the mysql file name including the .sql in its name*/
+            /*NOTE: Getting path to the Jar file being executed*/
+            /*NOTE: YourImplementingClass-> replace with the class executing the code*/
+            CodeSource codeSource = DBAutoBackupController.class.getProtectionDomain().getCodeSource();
+            File jarFile = new File(codeSource.getLocation().toURI().getPath());
+            String jarDir = jarFile.getParentFile().getPath();
+
+            /*NOTE: Creating Database Constraints*/
+            String dbName = "YourDBName";
+            String dbUser = "YourUserName";
+            String dbPass = "YourUserPassword";
+
+            /*NOTE: Creating Path Constraints for restoring*/
+            String restorePath = jarDir + "\\backup" + "\\" + s;
+
+            /*NOTE: Used to create a cmd command*/
+            /*NOTE: Do not create a single large string, this will cause buffer locking, use string array*/
+            String[] executeCmd = new String[]{"mysql", dbName, "-u" + dbUser, "-p" + dbPass, "-e", " source " + restorePath};
+
+            /*NOTE: processComplete=0 if correctly executed, will contain other values if not*/
+            Process runtimeProcess = Runtime.getRuntime().exec(executeCmd);
+            int processComplete = runtimeProcess.waitFor();
+
+            /*NOTE: processComplete=0 if correctly executed, will contain other values if not*/
+            if (processComplete == 0) {
+                JOptionPane.showMessageDialog(null, "Successfully restored from SQL : " + s);
+            } else {
+                JOptionPane.showMessageDialog(null, "Error at restoring");
+            }
+
+
+        } catch (URISyntaxException | IOException | InterruptedException | HeadlessException ex) {
+            JOptionPane.showMessageDialog(null, "Error at Restoredbfromsql" + ex.getMessage());
+        }
+
+    }
+
 }
