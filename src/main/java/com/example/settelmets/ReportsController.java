b5de7d6814d34f873d93d1cff9f1d4a2ea78f60b
Mohammed_Altujjar <mohammed_.1996@live.com>
2020-04-06 19:59:53 +0300

    Reports Update 4
    - download links added

--- src/main/java/com/example/settelmets/ReportsController.java
+++ src/main/java/com/example/settelmets/ReportsController.java
@@ -4,19 +4,10 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.net.URI;
-
-import javax.annotation.Resource;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.core.io.UrlResource;
 import org.springframework.data.repository.query.Param;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -25,9 +16,6 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.example.MQ.SettledChaque;
 import com.example.settelmets.SettlementService;
-import com.itextpdf.kernel.geom.Path;
-import com.itextpdf.kernel.pdf.canvas.parser.clipper.Paths;
-import com.itextpdf.styledxmlparser.css.media.MediaType;
 import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
 @RestController
@@ -39,91 +27,97 @@ public class ReportsController {
 	@Autowired
 	private SettlementService settlementService ;
 
-	@RequestMapping(value = "/downloaddocxFile", method = RequestMethod.GET)
-	public StreamingResponseBody getSteamingFile(HttpServletResponse response) throws IOException {
-		response.setContentType("application/octet-stream");
-		response.setHeader("Content-Disposition", "attachment; filename=\"D:\\fontstyle.docx\"");
-		InputStream inputStream = new FileInputStream(new File("D:\\fontstyle.docx"));
-		return outputStream -> {
-			int nRead;
-			byte[] data = new byte[1024];
-			while ((nRead = inputStream.read(data, 0, data.length)) != -1) {
-				System.out.println("Writing some bytes of file...");
-				outputStream.write(data, 0, nRead);
-			}
-		};
-	}
-
-	@RequestMapping(value = "/downloadpdfFile", method = RequestMethod.GET)
-	public StreamingResponseBody getSteamingFile2(HttpServletResponse response) throws IOException {
-		response.setContentType("application/pdf");
-		response.setHeader("Content-Disposition", "attachment; filename=\"D:\\h.pdf\"");
-		InputStream inputStream = new FileInputStream(new File("D:\\h.pdf"));
-		return outputStream -> {
-			int nRead;
-			byte[] data = new byte[1024];
-			while ((nRead = inputStream.read(data, 0, data.length)) != -1) {
-				System.out.println("Writing some bytes of file...");
-				outputStream.write(data, 0, nRead);
-			}
-		};
-	}
-
-
-
-	
 	@Autowired 
 	private ReportLinkService reportLinkService ; 
 	
-	@RequestMapping(method = RequestMethod.GET,value = "/settlement/checks/settled/reports/export/{id}")
-	public ModelAndView getExportIndex(@PathVariable int id ) {
+	
+	@RequestMapping(method = RequestMethod.GET,value = "/settlement/checks/settled/reports/export")
+	public ModelAndView getExportIndex(@Param(value ="id") int id ) {
 		SettledChaque settledCheck = this.settlementService.findCheckByID(id);
 		ModelAndView mav = new ModelAndView("DOC/exportHandler");
 		mav.addObject("check",settledCheck);
 		return mav ; 
 	}
 	
-	@RequestMapping(method = RequestMethod.GET , value = "/settlement/checks/settled/reports/export")
+	@RequestMapping(method = RequestMethod.GET , value = "/settlement/checks/settled/reports/export/html")
+	public ModelAndView getXmlReport(@Param(value ="id") int id) {
+		SettledChaque settledChaque = this.settlementService.findCheckByID(id) ; 
+		return this.reportsService.exportXml(settledChaque);
+	}
+	
+	@RequestMapping(method = RequestMethod.GET , value = "/settlement/checks/settled/reports/export/docx")
 	@ResponseBody
-	public void  getDocxReport(@Param(value ="id") int id){
+	public StreamingResponseBody getDocxReport(@Param(value ="id") int id , HttpServletResponse response ){
 		SettledChaque settledChaque = this.settlementService.findCheckByID(id) ; 
 		ReportsLinkModel reportsLinkModel = this.reportLinkService.getRportLinkModel(settledChaque.getId(),1) ; 	
 		String path ; 
+		try {
 		if(reportsLinkModel == null ) {
 			path = this.reportsService.exportDocx(settledChaque);
 			System.out.println("file path : "+path );
 			reportsLinkModel = new ReportsLinkModel(settledChaque.getId(),path,1);
 			this.reportLinkService.addReportLinkModel(reportsLinkModel,1);
+			return getSteamingDocxFile(response,path);
 		}else {
 			path = reportsLinkModel.getPath() ; 
+			return getSteamingDocxFile(response,path);
 		}
+		}catch(Exception e ) {
+			e.printStackTrace(); 
+			return null;	
+		}	
 	}
 	
-	@RequestMapping(method = RequestMethod.GET , value = "/settlement/checks/settled/reports/export/pdf/{id}")
-	public void getPdfReport(@PathVariable int id){
+	@RequestMapping(method = RequestMethod.GET , value = "/settlement/checks/settled/reports/export/pdf")
+	@ResponseBody
+	public StreamingResponseBody getPdfReport(@Param(value ="id") int id,HttpServletResponse response){
 		SettledChaque settledChaque = this.settlementService.findCheckByID(id) ; 
 		ReportsLinkModel reportsLinkModel = this.reportLinkService.getRportLinkModel(settledChaque.getId(),2) ; 	
 		String path ; 
+		try {
 		if(reportsLinkModel == null) {
 			path = this.reportsService.excportPDF(settledChaque);
 			System.out.println("file path : "+path );
 			reportsLinkModel = new ReportsLinkModel(settledChaque.getId(),path,2);
 			this.reportLinkService.addReportLinkModel(reportsLinkModel,2);
+			return this.getSteamingPdfFile(response, path);
 		}else {
 			path = reportsLinkModel.getPath() ; 
+			return this.getSteamingPdfFile(response, path);
 		}
-		
-	
-	
+		}catch(Exception e ) {
+			e.printStackTrace();  
+			return null ; 
+		}
 	}
 
 	
-	@RequestMapping(method = RequestMethod.GET , value = "/settlement/checks/settled/reports/export/xml/{id}")
-	public ModelAndView getXmlReport(@PathVariable int id) {
-		SettledChaque settledChaque = this.settlementService.findCheckByID(id) ; 
-		return this.reportsService.exportXml(settledChaque);
+	public StreamingResponseBody getSteamingDocxFile(HttpServletResponse response,String path) throws IOException {
+		response.setContentType("application/octet-stream");
+		response.setHeader("Content-Disposition", "attachment; filename=\"report.docx");
+		InputStream inputStream = new FileInputStream(new File(path));
+		return outputStream -> {
+			int nRead;
+			byte[] data = new byte[1024];
+			while ((nRead = inputStream.read(data, 0, data.length)) != -1) {
+				System.out.println("Writing some bytes of file...");
+				outputStream.write(data, 0, nRead);
+			}
+		};
 	}
 
-
+	public StreamingResponseBody getSteamingPdfFile(HttpServletResponse response,String path) throws IOException {
+		response.setContentType("application/pdf");
+		response.setHeader("Content-Disposition", "attachment; filename=\"report.pdf");
+		InputStream inputStream = new FileInputStream(new File(path));
+		return outputStream -> {
+			int nRead;
+			byte[] data = new byte[1024];
+			while ((nRead = inputStream.read(data, 0, data.length)) != -1) {
+				System.out.println("Writing some bytes of file...");
+				outputStream.write(data, 0, nRead);
+			}
+		};
+	}
 	
 }
