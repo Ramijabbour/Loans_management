06c9cc545a91a3b96e7b54c042663344ba31b758
Mohammed_Altujjar <mohammed_.1996@live.com>
2020-04-16 07:34:27 +0300

    Bank Status Update
    - Banks stats view added At /Banks/view/stats/{id}
    - can be reached from bank view table from view button
    - banj status model added
    - bank stats Service added
    - view needs to be replaced

---
+++ src/main/java/com/example/Banks/BankStatsService.java
@@ -0,0 +1,47 @@
+package com.example.Banks;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.Allocations.AllocationsService;
+import com.example.CloseLoans.CloseLoanService;
+import com.example.Loans.LoanService;
+import com.example.OpenLoans.OpenLoansService;
+
+@Service 
+public class BankStatsService {
+
+	@Autowired 
+	private BankService bankService ; 
+	
+	@Autowired 
+	private AllocationsService allocationsService ;
+	
+	@Autowired 
+	private LoanService loansService ; 
+	
+	@Autowired
+	private CloseLoanService closedLoansService ; 
+	
+	@Autowired 
+	private OpenLoansService openLoansService ; 
+	
+	
+	
+	public BankStatsService() {
+	}
+	
+	public StatsModel getBankStats(Banks bank) {
+		StatsModel statusModel = new StatsModel(); 
+		statusModel.setBank(bank);
+		statusModel.setBranchesList(this.bankService.getBankBranches(bank));
+		statusModel.setAllAllocations(this.allocationsService.getBankAllocations(bank));
+		statusModel.setOverAllLoansList(this.loansService.getBankLoans(bank));
+		statusModel.setOpenLoansList(this.openLoansService.getBankOpenLoans(bank));
+		statusModel.setClosedLoansList(this.closedLoansService.getBankClosedLoans(bank));
+		statusModel.calculateBankStats(); 
+		return statusModel; 
+	}
+	
+	
+}
---
+++ src/main/java/com/example/Banks/BanksStatsController.java
@@ -0,0 +1,30 @@
+package com.example.Banks;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class BanksStatsController {
+
+	
+	@Autowired
+	private BankStatsService bankStatsService ; 
+	
+	@Autowired
+	private BankService banksService ; 
+	
+	@RequestMapping(method = RequestMethod.GET , value = "/banks/view/stats")
+	public ModelAndView getBankStats(@Param(value ="id") int id) {
+		ModelAndView mav = new ModelAndView("Banks/stats");
+		Banks bank = this.banksService.getBankById(id);
+		mav.addObject("bankstats",this.bankStatsService.getBankStats(bank)) ;
+		return mav ; 
+	}
+	
+	// add redirect routes 
+	
+}
