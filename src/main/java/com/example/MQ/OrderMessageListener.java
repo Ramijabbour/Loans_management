f8a802fb913a4f20b6d8a53638621497d1ab2ccf
Mohammed_Altujjar <mohammed_.1996@live.com>
2020-09-12 22:06:26 +0300

    Update OrderMessageListener.java

--- src/main/java/com/example/MQ/OrderMessageListener.java
+++ src/main/java/com/example/MQ/OrderMessageListener.java
@@ -1,22 +1,18 @@
+  
 package com.example.MQ;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
-
-import com.example.SiteConfig.MasterService;
 import com.example.settelmets.Models.Chaque;
 import com.example.settelmets.Repositories.OnHoldCheckRepository;
 
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class OrderMessageListener {
-/*
+
     @Autowired
     private OnHoldCheckRepository onHoldCheckRepository;
     
@@ -24,16 +20,14 @@ public class OrderMessageListener {
     
 
     static final Logger logger = LoggerFactory.getLogger(OrderMessageListener.class);
-    @RabbitListener(queues = RabbitConfig.QUEUE_ORDERS)
+    @RabbitListener(queues = "CheckQueue")
     public void processOrder(Chaque check) {
     	System.out.println("check recieved with info : "+check);
-        //System.out.println(check.getAmount()+" "+check.CheckId+" "+check.FirstBank+" "+check.SecondBank+" "+check.active);
-
-        //check.localDateTime= MasterService.getCurrDateTime();
-        //onHoldCheckRepository.save(check);
+    	check.setSent(false);
+        onHoldCheckRepository.save(check);
         logger.info("Order Received: "+check);
     }
-    */
+    
 //    @Scheduled(fixedRate = 7200000)
 //    public void reportCurrentTime() {
 //       return;
